7770ee93030911983ee15385dd5e832488b6d4c7
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-08-31 08:52:10 +0000

    push this one too

---
+++ next13/components/TestingForBoys.tsx
@@ -0,0 +1,13 @@
+import { searchBoysProps, searchMeProps } from '@/types'
+import React from 'react'
+
+export const TestingForBoys = ({ king, }:searchBoysProps ) : number [] => {
+
+  return [23]
+}
+
+
+const kwameDushBug =({array}:searchMeProps<string>)=>{
+
+    array = ['dushBugs ']
+}
--- next13/types/index.ts
+++ next13/types/index.ts
@@ -1,11 +1,24 @@
 import { MouseEventHandler } from "react";
 
-
-export interface  CustomBtnProps {
-title: string;
-containerStyles?: string;
-handleClick?: MouseEventHandler <HTMLButtonElement> 
-btnType?: 'button'| 'submit'
+export interface CustomBtnProps {
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
 }
 
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
 
+export interface searchBoysProps {
+  king: () => boolean;
+  array: string[];
+}
+
+// so to return type give a semicolon and write the type you wanna return
+
+export interface searchMeProps<T> {
+  array: T[];
+}
